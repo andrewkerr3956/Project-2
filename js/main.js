9abe514a7187fbfe88174bb077345d28606779ce
Andrew Kerr <andrewkerr3956@gmail.com>
2021-08-26 23:51:12 -0400

    Fetch function returns an array of gif objects and the gif will be displayed for each object.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-window.onload = () => {
-    // Grab the API key and the element (input textbox) in which the user typed something
+window.addEventListener('load', () => {
+    // Grab the API key
     let api_key = 'P008Twl6jsjXcFQn9lQSzLrTQRkkYTgT';
-    let search_query = document.getElementById('search-query').value;
-    console.log(search_query);
-    /* Grab the type of results the user wants to see. Hard-coded value for now, but will
-    have functionality added later. */
-    let resultType = 'gifs';
+
+    // Get the search button by ID and then add a click event listener to it
+    let fetchGiphy = async(search_query, resultType) => {
+        let giphyResult = await fetch(`http://api.giphy.com/v1/${resultType}/search?api_key=${api_key}&q=${search_query}`)
+        let data = await giphyResult.json();
+        console.log(data);
+        for(let x = 0; x < data.data.length; x++) {
+            document.body.innerHTML += `<img src='${data.data[x].images.original.url}' />`;
+        }
+    };
+
 
     const searchButton = document.getElementById('search-button');
     searchButton.addEventListener('click', () => {
-        let giphyResult = fetch(`http://api.giphy.com/v1/${resultType}/search?api_key=${api_key}&q=${search_query}`);
-        console.log(giphyResult);
+        let search_query = document.getElementById('search-query').value;
+        let resultType = 'gifs';
+        fetchGiphy(search_query, resultType);
     });
+    
 
-}
+
+});
