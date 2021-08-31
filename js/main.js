5143e415e19575d5f0869ecbcba5b24e3dee0deb
Andrew Kerr <andrewkerr3956@gmail.com>
2021-08-31 15:58:40 -0400

    Footer positioning now aligns properly based on if there are any GIPHY results. Adjusted username displayed in card to make it less noticeable if there is no username attached to GIF. Flexbox output now displays a message if no search results were found.

--- js/main.js
+++ js/main.js
@@ -8,14 +8,19 @@ window.addEventListener('load', () => {
         let giphyResult = await fetch(`http://api.giphy.com/v1/${resultType}/search?api_key=${api_key}&q=${search_query}`)
         let data = await giphyResult.json();
         console.log(data);
-        loopGiphyResults(data);
+        if (data.data.length > 0) {
+            loopGiphyResults(data);
+        }
+        else {
+            document.getElementById('flexbox').innerHTML = '<h1>No results found.</h1>';
+        }
     };
 
     let loopGiphyResults = async (data) => {
         const output = document.getElementById('flexbox');
         /* Clearing the inner HTML of flexbox div is important because otherwise
         when user searches for more gifs or stickers, then it would stack on top of the
-        previous searches if the inner HTML is not cleared*/ 
+        previous searches if the inner HTML is not cleared*/
         output.innerHTML = '';
         /* Through the use of Bootstrap cards, we can display some information about
         the gifs/stickers rather than just displaying them. */
@@ -26,13 +31,13 @@ window.addEventListener('load', () => {
             </div>
             <div class='card-footer'>
                 <h4>${data.data[x].title}</h4> 
-                <p>Posted by ${data.data[x].username}</p>
-                </div>`
-           ;
+                <p>${data.data[x].username}</p>
+                </div>`;
         }
         // Clear out the results of the fetch as well as the data received from it
         giphyResult = '';
         data = '';
+        document.getElementById('footer').style.position = 'relative';
     }
 
 
@@ -40,6 +45,7 @@ window.addEventListener('load', () => {
     searchButton.addEventListener('click', () => {
         let search_query = document.getElementById('search-query').value;
         let resultType = 'gifs';
+        document.getElementById('footer').style.position = 'absolute';
         fetchGiphy(search_query, resultType);
     });
 
